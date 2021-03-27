45284f7fc7d18b1b8760a8c0d7903a893a192deb
Alvis Lu <uualvislu@gmail.com>
2021-03-27 17:54:13 +0800

    Add index reouter

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import mondel, { connectDB } from './db';
+import Router from './routers';
 
 const app = express();
 const db = mondel.connection;
@@ -7,6 +8,9 @@ const db = mondel.connection;
 db.once('open', () => {
   console.log(`[MongoDB] Connection opend.`);
 });
+
+app.use('/', Router);
+
 connectDB();
 
 export default app;
