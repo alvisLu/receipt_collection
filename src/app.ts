9a89058f1e97ffa3563879f000ced7180e3f8de9
Alvis Lu <uualvislu@gmail.com>
2021-03-29 15:32:58 +0800

    rename db -> models, add

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import mondel, { connectDB } from './db';
+import mondel, { connectDB } from './models';
 import Router from './routers';
 
 const app = express();
@@ -8,9 +8,8 @@ const db = mondel.connection;
 db.once('open', () => {
   console.log(`[MongoDB] Connection opend.`);
 });
+connectDB();
 
 app.use('/', Router);
-
-connectDB();
 
 export default app;
